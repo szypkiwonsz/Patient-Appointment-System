043503f53d72ddf5701bb8287172de5d7d154089
szypkiwonsz <szypkiwonsz@github.com>
2020-02-07 22:19:17 +0100

    commit

--- appointment/urls.py
+++ appointment/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='appointment-index'),
     path('uslugi/', views.uslugi, name='appointment-uslugi'),
-    path('cennik/', views.uslugi, name='appointment-cennik'),
-    path('o_mnie/', views.uslugi, name='appointment-o_mnie'),
+    path('cennik/', views.cennik, name='appointment-cennik'),
+    path('o_mnie/', views.o_mnie, name='appointment-o_mnie')
 ]
