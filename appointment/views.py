4cab3a6b142e6860dcd009e15031171cc7733951
szypkiwonsz <szypkiwonsz@github.com>
2020-02-09 15:34:42 +0100

    Appointment view added

--- appointment/views.py
+++ appointment/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+# from .forms import DateForm
+from .forms import AppointmentForm
 
 
 # Create your views here.
@@ -16,3 +19,20 @@ def cennik(response):
 
 def o_mnie(response):
     return render(response, 'appointment/o_mnie.html')
+
+
+def wizyty(request):
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            date = form.cleaned_data.get('date')
+            if date.hour == 0:
+                messages.warning(request, f'Nie można wybrać tej godziny!')
+                return redirect('appointment-wizyty')
+            else:
+                form.save()
+                messages.success(request, f'Poprawnie umówiłeś wizytę.')
+                return redirect('appointment-index')
+    else:
+        form = AppointmentForm()
+    return render(request, 'appointment/wizyty.html', {'form': form})
