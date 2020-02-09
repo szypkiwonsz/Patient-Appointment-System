31aa2a3c600a0925a628389a5c44acc1b1814798
szypkiwonsz <szypkiwonsz@github.com>
2020-02-09 15:33:33 +0100

    Crispy forms added

--- Patient_Appointment_System/settings.py
+++ Patient_Appointment_System/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'appointment.apps.AppointmentConfig',
     'django.contrib.admin',
+    'crispy_forms',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
