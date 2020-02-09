c6eb8770715d87a91f9911205a0f812728fe5cf7
szypkiwonsz <szypkiwonsz@github.com>
2020-02-09 15:32:36 +0100

    Appointment model added

--- appointment/models.py
+++ appointment/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    date = models.DateTimeField()
+    name = models.CharField("Podaj swoje imię:", max_length=120)
+    email = models.EmailField("Podaj adres email:")
+
+    def __str__(self):
+        return self.email
