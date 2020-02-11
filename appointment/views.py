c874f93c7a0e6eaf91bc355bbbf50986f00be6af
szypkiwonsz <szypkiwonsz@github.com>
2020-02-11 19:37:09 +0100

    Added checking if the date is busy or the email is already saved.

--- appointment/views.py
+++ appointment/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 # from .forms import DateForm
 from .forms import AppointmentForm
-
+from .models import Post
 
 # Create your views here.
 def index(response):
@@ -25,19 +25,33 @@ def wizyty(request):
     if request.method == 'POST':
         form = AppointmentForm(request.POST)
         if form.is_valid():
-            date = form.cleaned_data.get('date')
+            date_selected = form.cleaned_data.get('date')
             name = form.cleaned_data.get('name')
-            if date.hour == 0:
-                messages.warning(request, f'Proszę wybrać godzinę.')
+            email_selected = form.cleaned_data.get('email')
+
+            if date_selected.hour == 0:
+                messages.warning(request, f'Proszę wybrać poprawną godzinę.')
                 return redirect('appointment-wizyty')
+
             # Checks if first name is typed correctly without any numbers etc.
             elif name.isalpha() is not True:
                 messages.warning(request, f'Niepoprawne imię.')
                 return redirect('appointment-wizyty')
+
             else:
+                date = Post.objects.raw('select * FROM appointment_post')
+                for i in date:
+                    if date_selected == i.date:
+                        messages.warning(request, f'Wybrana data jest już zajęta.')
+                        return redirect('appointment-wizyty')
+                email = Post.objects.raw('select * FROM appointment_post')
+                for i in email:
+                    if email_selected == i.email:
+                        messages.warning(request, f'Wybrany email jest już umówiony na wizytę.')
+                        return redirect('appointment-wizyty')
                 form.save()
                 messages.success(request, f'Poprawnie umówiłeś wizytę.')
-                return redirect('appointment-index')
+                return redirect('appointment-wizyty')
     else:
         form = AppointmentForm()
     return render(request, 'appointment/wizyty.html', {'form': form})
