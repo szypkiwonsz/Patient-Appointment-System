2180e68d0fe19ff19fbabe0677d8dd9d74b0c5b3
szypkiwonsz <szypkiwonsz@github.com>
2020-02-14 13:24:39 +0100

    Added appointment cancel and confirm cancel form

--- appointment/forms.py
+++ appointment/forms.py
@@ -9,3 +9,11 @@ class AppointmentForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['date', 'name', 'email']
+
+
+class AppointmentCancel(forms.Form):
+    email = forms.EmailField(label='Podaj adres email podany przy rejestracji wizyty:')
+
+
+class AppointmentCancelConfirm(forms.Form):
+    key = forms.CharField(label='Podaj kod wysłany na podany adres email, by potwierdzić odwołanie wizyty.')
