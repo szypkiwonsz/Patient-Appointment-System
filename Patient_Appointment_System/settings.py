86d931bfd1386957bc7e6394e293737fc6fabef9
szypkiwonsz <szypkiwonsz@github.com>
2020-02-16 22:57:34 +0100

    Added data for sending emails

--- Patient_Appointment_System/settings.py
+++ Patient_Appointment_System/settings.py
@@ -123,3 +123,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'YOUR EMAIL ADRESS'
+EMAIL_HOST_PASSWORD = 'PASSWORD'
+EMAIL_PORT = 587
